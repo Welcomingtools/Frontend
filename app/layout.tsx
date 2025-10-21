d50f195ac23fabce17d6afd9fac0bbce94893c51
JessBhad <2663997@students.wits.ac.za>
2025-10-22 00:07:20 +0200

    Addition of app logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,9 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "MSS Welcoming Team App",
   description: "Mobile application for the Mathematical Sciences Support Welcoming Team at Wits University",
-  generator: 'v0.dev'
+  generator: 'v0.dev',
+  icons:"/images/MSS Tab logo.png",
+
 }
 
 export default function RootLayout({
@@ -19,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/images/MSS Tab logo.png" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
